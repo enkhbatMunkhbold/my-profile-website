0eadf4cea78381815dfb477449b9c552b8a2675f
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-12-08 14:31:28 -0800

    Current Status

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleIt() {
-  let toggleBtn = document.getElementsByClassName("btn")
-  if(toggleBtn.display === 'none') {
-    toggleBtn.display = 'block'
-  } else {
-    toggleBtn.display = 'none'
-  }
-}
